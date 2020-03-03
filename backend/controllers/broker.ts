79178856bbd90fd837af88d2737ab787c3ac0a39
Magnus Thor <magnus.thor74@gmail.com>
2020-03-04 00:34:49 +0100

    Licence update, minor fixes

--- backend/controllers/broker.ts
+++ backend/controllers/broker.ts
@@ -5,10 +5,21 @@ import {
     CanSet
 } from 'thor-io.vnext'
 
-
-@ControllerProperties("broker", false, 2000)
+// Controller will be know as "broker", and not seald ( seald = true, is background sevices),
+// controller (broker) will pass a heartbeat to client each 2 seconds to keep alive. 
+@ControllerProperties("broker", false, 2*1000)
 export class Broker extends ThorIO.Controllers.BrokerController {
     constructor(connection: ThorIO.Connection) {
         super(connection);
     }
+    // extend 'broker' with a custom method 'foo' -> client calls "foo" and controller (broker) passes data to all in "bar"
+    @CanInvoke(true)  // use decorators to make methods public to client.
+    foo(data:any){
+        this.invokeToAll(data,"bar");
+    }
+
+    // this property can be set from client, by calling set property, and then be use to filter clients based  on it state (property)
+    @CanSet(true)
+    age:number
+
 }
