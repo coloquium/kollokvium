cadc3c6f0afa309a27618f979dfb74930029ec82
Tibi Covaci <tibi@covaci.se>
2020-04-04 22:07:40 +0200

    Webpack build for ts

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,29 @@
+var path = require('path');
+
 module.exports = {
   mode:"production", 
   watch: false,
   entry: {    
-    kollovium :'./client/app.js',
+    kollkovium : path.join(__dirname, 'client', 'app.ts'),
   },
-  
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: {
+          loader: 'ts-loader',
+          options: {
+            allowTsInNodeModules: 'true'
+          }
+        }
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
   output: {
-    path: __dirname + '/build',
+    path: path.resolve(__dirname, 'build'),
     filename: '[name]-bundle.js'
   },
 }
