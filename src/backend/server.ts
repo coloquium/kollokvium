d1646fcee6a3c53892928b6e9ae74ab0d814dbbd
Magnus Thor <magnus.thor74@gmail.com>
2020-04-07 21:32:28 +0200

    Duplicate id fix

--- src/backend/server.ts
+++ src/backend/server.ts
@@ -15,8 +15,7 @@ let RTC = new ThorIO(
 
 require("express-ws")(app);
 
-
-app.use("/",express.static("./client")); 
+app.use("/",express.static(path.join(process.cwd(),"/dist/client"))); 
 
 app.ws("/", function (ws, req) {
     RTC.addWebSocket(ws, req);
