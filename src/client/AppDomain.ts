da7601ac30929b762b26bef270a98e3ba8680729
Magnus Thor <magnus.thor74@gmail.com>
2020-04-07 13:31:14 +0200

    appDomain fix (logging)

--- src/client/AppDomain.ts
+++ src/client/AppDomain.ts
@@ -6,8 +6,6 @@
  */
 
 const appConfig = require("./settings.json");
-
-
 export class AppDomain {
     version: string;
     serverUrl: string;
@@ -18,17 +16,26 @@ export class AppDomain {
         return `${this.contextPrefix}-${value}`;
     }
     getHost() {
-        const port = location.port;
+        const port = window.location.port;
         const host = (appConfig.host || window.location.hostname)
-        return location.protocol + "//" + host + (port.length > 0 ? ":" + port : "");
+        let result =  location.protocol + "//" + host + (port.length > 0 ? ":" + port : "");
+        console.log(`host resolved ${result}`);
+        return result;
+
     }
     getServerUrl() {
-        const port = location.port;
+        const port = window.location.port;
         const host = (appConfig.serverUrl || window.location.hostname);
-        return location.protocol.includes("https:") ? "wss://" : "ws://" + host
+        let result =  location.protocol.includes("https:") ? "wss://" : "ws://" + host
             + (port.length > 0 ? ":" + port : "");
+        console.log(`serverUrl resolved ${result}`);
+        return result;
+
     }
     constructor() {
+
+        console.info("appConfig loaded", appConfig);
+
         this.host = this.getHost(); // location.protocol +"//" + (appConfig.host || window.location.hostname);
         this.domain = appConfig.domain;
         this.contextPrefix = appConfig.contextPrefix;
