faea0b734a2772a6d8ab2e412022171f6805f10b
Magnus Thor <magnus.thor74@gmail.com>
2020-04-07 14:21:23 +0200

    AppDomain scheme fixed for serverUrl

--- src/client/AppDomain.ts
+++ src/client/AppDomain.ts
@@ -19,28 +19,21 @@ export class AppDomain {
         const port = window.location.port;
         const host = (appConfig.host || window.location.hostname)
         let result =  location.protocol + "//" + host + (port.length > 0 ? ":" + port : "");
-        console.log(`host resolved ${result}`);
         return result;
-
     }
     getServerUrl() {
         const port = window.location.port;
         const host = (appConfig.serverUrl || window.location.hostname);
-        let result =  location.protocol.includes("https:") ? "wss://" : "ws://" + host
+        const scheme =  location.protocol.includes("https:") ? "wss://" : "ws://";
+        let result =  scheme + host
             + (port.length > 0 ? ":" + port : "");
-        console.log(`serverUrl resolved ${result}`);
         return result;
-
     }
     constructor() {
-
-        console.info("appConfig loaded", appConfig);
-
         this.host = this.getHost(); // location.protocol +"//" + (appConfig.host || window.location.hostname);
         this.domain = appConfig.domain;
         this.contextPrefix = appConfig.contextPrefix;
         this.serverUrl = this.getServerUrl()
         this.version = appConfig.version;
-
     }
 }
