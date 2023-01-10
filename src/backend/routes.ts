ae66df377a9007565f8d496bcea88f01d84bf042
Magnus 'Bagzy' Thor <magnus.thor74@gmail.com>
2023-01-10 19:51:35 +0100

    catch error and return

--- src/backend/routes.ts
+++ src/backend/routes.ts
@@ -3,12 +3,18 @@ import express from 'express';
 import { openAIRequest } from './openAIRequest';
 export const router = express.Router();
 
-router.post("/translate/openai/",async (req,res)=> {
+router.post("/translate/openai/", async (req, res) => {
     const { prompt } = req.body;
-    const result = await openAIRequest(prompt);
-    res.send(result);
+    const request = openAIRequest(prompt);
+    request.then(aiResponse => {
+        res.send(aiResponse);
+    }).catch(err => {
+        res.status(500);
+        res.send(err);
+    });
+
 });
 
 
-    
+
 
